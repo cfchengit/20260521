e2190e3b0390911218733bd03e22a9653dd626f8
cfchen_tku <cfchen@mail.tku.edu.tw>
2026-05-21 11:20:43 +0800

    1.01

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/opendata-map/'  // ← 改成你的 GitHub Repository 名稱
+  base: '/20260521/'  // ← 改成你的 GitHub Repository 名稱
 })
